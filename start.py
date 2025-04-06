146f7938141c3e5a8b471cd0c20b363c5d90b2cb
JarLane <151280880+JarLane@users.noreply.github.com>
2025-04-06 18:10:23 -0400

    Update start.py

--- start.py
+++ start.py
@@ -3,8 +3,18 @@ import requests
 import ssl
 import json
 
-url = "change this url"
-response = requests.get(url)
-if response.status_code == 200:
-    pass
-    else
+url = "http://localhost:33333"
+try:
+    response = requests.get(url)
+    if response.status_code == 200:
+        pass
+    else:
+        path = "problem.json"
+        with open(path,"w") as file:
+            json_data = response.json()
+            json.dump(json_data, file, indent=2)
+except Exception as e:
+    path = "problem.json"
+    with open(path,"w") as file:
+        json_data = {'Error' : str(e)}
+        json.dump(json_data, file, indent=2)
