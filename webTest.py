5d6776a5d03680ae2c706711d1a3b0f28feda828
JarLane <151280880+JarLane@users.noreply.github.com>
2025-04-06 19:40:06 -0400

    Update webTest.py

--- webTest.py
+++ webTest.py
@@ -17,4 +17,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=33333, debug=True)
+    app.run(host='0.0.0.0', port=33333, debug=True, ssl_context=('cert.pem', 'key.pem'))
