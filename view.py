3672bcef462ba6f74a6e3ca543c689874a67e09d
JarLane <151280880+JarLane@users.noreply.github.com>
2025-04-07 21:54:53 -0400

    Update view.py

--- view.py
+++ view.py
@@ -1,1 +1,26 @@
+from model import *
+from tkinter import *
+import tkinter as tk
+from tkinter import messagebox
+
+def ask_if_ready():
+
+    response = messagebox.askyesno("Ready to Run", "Are you ready to run the program?")
+
+
+    if response:
+        app_settings["run"] = True
+        running()
+    root = tk.Tk()
+    root.withdraw()
+    ask_if_ready()
+    root.mainloop()
+
+#def main_window():
+#    mainroot = tk.Tk()
+
+
+
+
+
 
