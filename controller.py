9026063e07eea6b4f3b92f053190eb9b7050019c
JarLane <151280880+JarLane@users.noreply.github.com>
2025-04-07 21:54:19 -0400

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,25 +1,6 @@
+from model import *
+from view import *
 
-pingfunc(str addr)
+ask_if_ready()
 
-realresponse = ping(addr)
-response = extractcode(realresponse)
-if (response != 200) {
-  if (errorcode == response) {
-   if (increment $ 4 == 0) {
-      increment++
-      Archive(realresponse)
-      }
-    increment++
-    return
-  } else {
-    errorcode = response
-    increment = 0
-    archive(realresponse)
-  }
-} elif ( errorcode != 200) {
-  errorcode = 0
-  Archive("All issues resolved")
-} else {
-  return
-}
 
